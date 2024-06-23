4b1130d40620560517fcae59ae2101e649561ea5
Satvik Muddana <satvikmuddana@berkeley.edu>
2024-06-23 01:23:04 -0700

    Updates

---
+++ static/js/952.09b2559f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunklecture_assistant=self.webpackChunklecture_assistant||[]).push([[952],{4952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.09b2559f.chunk.js.map
